7962dd33a5e86483f292b5848fec454738bddab8
Ryan Heathcote <rheathcote@sharptop.co>
2017-10-23 10:08:52 -0400

    added a while loop.

--- words_api.sh
+++ words_api.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-curl --get --include 'https://wordsapiv1.p.mashape.com/words/?random=true' \
-  -H "$WORDS_API_KEY_HEADER" \
-  -H 'Accept: application/json'
+while true
+do
+    curl --get --include 'https://wordsapiv1.p.mashape.com/words/?random=true' \
+      -H "$WORDS_API_KEY_HEADER" \
+      -H 'Accept: application/json'
+done
